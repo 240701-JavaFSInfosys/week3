ff946312bd718f9b5c5bc87486acbb1eee9bc3e4
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-07-17 15:06:33 -0400

    maybe the real streams are the multiple detours we took along the way

--- HelloThreadsStreams/src/com/revature/Launcher.java
+++ HelloThreadsStreams/src/com/revature/Launcher.java
@@ -1,5 +1,12 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Launcher {
 
     public static void main(String[] args) {
@@ -27,6 +34,52 @@ public class Launcher {
         //run a third thread - note that it doesn't matter if we use t1 or t2.
         Thread thread3 = new Thread(t1);
         thread3.start();
+
+        System.out.println("===================(Streams)");
+
+        //Create a Stream of Integers
+        IntStream.range(0, 10).skip(3).forEach(System.out::println);
+        /*The stream will have values 1-9 (range() - intermediate)
+         The stream will skip the first 3 values (skip() - intermediate)
+         FOR EACH value in the stream, print the value(forEach() - terminal)
+
+         What is System.out::println? It's called "method reference syntax" (in week 2 of the curriculum)
+         You may notice we never said what value to print in the print statement
+         Method reference syntax let's us imply the value that a method will take as a parameter*/
+
+        //Stream.of() is a convenient way to create a Stream of any object. The data must be of the same type
+        Optional<String> string = Stream.of("ok", "ummm", "Ben", "Revature", "Scooby Doo", "Alright")
+                .sorted() //this will sort the elements in their natural order (alphabetical in this case)
+                .findFirst(); //this will return the first element in the Stream as an OPTIONAL
+
+        /*ok what is an Optional? It's a Java class that will either hold a value or be empty
+        it will OPTIONALLY hold a value. This is useful when we potentially have a null value
+        great way to avoid NullPointerExceptions, but we need to extract the value in a certain way*/
+        if(string.isPresent()){
+            System.out.println(string.get());
+        }
+        //we've effectively found the first alphabetical value in the stream
+
+        //Let's use a Stream on a predefined ArrayList to create a new ArrayList
+        List<String> names = new ArrayList<>(); //downcasting to ArrayList - we can treat this List as an ArrayList
+
+        names.add("Ben");
+        names.add("Jamie");
+        names.add("Sai");
+        names.add("Nathan");
+        names.add("Ted");
+        names.add("Scooby Doo");
+        names.add("Scooby Doo");
+        names.add("Revature");
+
+        List<String> newNames = names.stream()
+                .distinct() //knocks out any duplicate values (the first Scooby Doo)
+                .sorted()
+                .filter(element -> element.length() < 10) //keep elements less than 10 characters (the 2nd Scooby)
+                .toList();
+
+        //print out the processed names
+        System.out.println(newNames);
 
     }
 }
